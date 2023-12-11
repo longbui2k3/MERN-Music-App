e6ff7021eaedf09e7b01316b82cd4cd4e5cf6fa2
longbui2k3 <buiduclongit@gmail.com>
2023-12-11 21:40:47 +0700

    Cap Nhat

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,11 @@
-import { PageResetPassword } from "./components";
+import { PageSignUp, PageResetPassword, PageLogin } from "./components";
 import { ChakraProvider } from "@chakra-ui/react";
-import SignUp from "./pages/SignUp";
-import PageForgotPassword from "./components/PageForgotPassword";
 export function App() {
   return (
     <ChakraProvider>
-      <div>
-        {/* <SignUp/> */}
-        {/* <PageResetPassword /> */}
-        <PageForgotPassword />
-      </div>
+      <PageLogin></PageLogin>
+      {/* <PageSignUp></PageSignUp> */}
+      {/* <PageResetPassword></PageResetPassword> */}
     </ChakraProvider>
   );
 }
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,1 +1,4 @@
 export { default as PageResetPassword } from "./PageResetPassword";
+export { default as PageForgotPassword } from "./PageForgotPassword";
+export { default as PageSignUp } from "./PageSignUp";
+export { default as PageLogin } from "./PageLogin";
