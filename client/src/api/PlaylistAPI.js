446e404f1e8fc1da8eed1203b0e853ac705fb754
longbui2k3 <buiduclongit@gmail.com>
2024-01-01 14:06:13 +0700

    cập nhật tiếp

---
+++ client/src/api/PlaylistAPI.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+export const getAllPlaylists = async () => {
+  const res = await axios.get(`http://localhost:4000/api/v1/playlist`);
+  return res;
+};
+export const getPlaylist = async (id) => {
+  console.log("Long");
+  const res = await axios.get(
+    `http://localhost:4000/api/v1/playlist/6573ecf5018c9cbc567f72bf/${id}`
+  );
+  return res;
+};
